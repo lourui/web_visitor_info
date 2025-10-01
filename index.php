c39a938945febd0fda8b19206ac52d554f6a8383
Louis Ruiz <louis_ruiz@hotmail.com>
2025-09-30 23:00:54 -0700

    Update

--- index.php
+++ index.php
@@ -31,14 +31,31 @@ $DaHtml = "
 <html>
 <head>
 <title>Web Visitor Info</title>
+
+    <!-- Start DataTables CSS and JS-->
+    <link href='https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css' rel='stylesheet'>
+     
+    <script src='https://code.jquery.com/jquery-3.7.1.js'></script>
+    <script src='https://cdn.datatables.net/2.3.4/js/dataTables.js'></script>
+
+
+
+    <!-- End DataTables CSS and JS-->
+
+
 </head>
 
 <body>
     <center><h2>Web Visitor Info</h2></center>
 
     <center>
-    <table>
+    <table id='myTable' class='display' style='width:90%; max-width:1200px;'>
+    <thead>
+    <tr>
     <th>IP Address</th> <th>User Agent</th> <th>Referrer</th> <th>Page Visited</th>
+    </tr>
+    </thead>
+
     <tr>
     <td>$ip_address</td> <td>$user_agent</td> <td>$referrer</td> <td>$page_visited</td>
     </tr>
@@ -48,6 +65,11 @@ $DaHtml = "
     </center>
     
 </body>
+
+<script>
+new DataTable('#myTable');
+//$('#myTable').DataTable();
+</script>
 
 
 
