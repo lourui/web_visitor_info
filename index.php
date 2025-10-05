12b5e4f27ced2ecabdb94e06e461b0db0882cf3e
Louis Ruiz <louis_ruiz@hotmail.com>
2025-10-04 21:47:21 -0700

    Added Function and DB

--- index.php
+++ index.php
@@ -9,6 +9,49 @@ This is to expose all possible web visitor data. This can later be inserted to a
 //var_dump($_SERVER);
 
 
+$servername = "localhost";
+$username = "demos";
+$password = "ThisIsAHardPassword2024";
+$dbname = "lourui_demos";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+//website statistics
+function trackVisitor($conn) {
+    // Capture IP Address
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+
+    // Capture User Agent
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+    // Capture Referrer (if available)
+    $referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Direct';
+
+    // Capture Page Visited
+    $page_visited = $_SERVER['REQUEST_URI'];
+
+
+
+    // Insert the data into the web_statistics table
+    $sql = "INSERT INTO web_statistics (ip_address, user_agent, referrer, page_visited) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $ip_address, $user_agent, $referrer, $page_visited);
+    $stmt->execute();
+}
+
+
+
+trackVisitor($conn);
+
+
     // Capture IP Address
     $ip_address = $_SERVER['REMOTE_ADDR'];
 
@@ -47,6 +90,7 @@ $DaHtml = "
 
 <body>
     <center><h2>Web Visitor Info</h2></center>
+    <center>Current Visitor</center>
 
     <center>
     <table id='myTable' class='display' style='width:90%; max-width:1200px;'>
@@ -78,6 +122,10 @@ new DataTable('#myTable');
 ";
 
 echo $DaHtml;
+
+
+
+
 
 
 ?>
